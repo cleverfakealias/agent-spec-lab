b538c3f6ac6502eea84ed03ec3f8d3cef9376668
BJH <me@benhickman.dev>
2025-10-01 00:18:44 -0500

    init change

--- src/agent_spec_lab/nodes/__init__.py
+++ src/agent_spec_lab/nodes/__init__.py
@@ -3,8 +3,7 @@
 from agent_spec_lab.nodes.answer import create_answer_node
 from agent_spec_lab.nodes.classifier import create_classifier_node
 from agent_spec_lab.nodes.confidence import create_confidence_node
-
-# from agent_spec_lab.nodes.fallback import create_fallback_node
+from agent_spec_lab.nodes.fallback import create_fallback_node
 from agent_spec_lab.nodes.reformulator import create_reformulator_node
 from agent_spec_lab.nodes.retriever import create_retrieve_node
 
@@ -12,7 +11,7 @@ __all__ = [
     "create_answer_node",
     "create_classifier_node",
     "create_confidence_node",
-    # "create_fallback_node",
+    "create_fallback_node",
     "create_reformulator_node",
     "create_retrieve_node",
 ]
